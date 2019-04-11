433855a9f196c12ba7d02ab61a01a8b3a6fe426c
David Mark Clements <huperekchuno@googlemail.com>
2019-04-11 15:58:12 +0200

    warning

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const { _compile } = Module.prototype
 require('esx') //load esx into the cache
 const esx = require.resolve('esx')
 
+process.emitWarning('esx/optimize is currently an experimental feature please expect breakage and report issues!')
+
 Module.prototype._compile = function (content, filename) {
   if (filename === esx) return _compile.call(this, content, filename)
   const converted = convert(content)
-  return _compile.call(this, converted, filename)
+  try { 
+    return _compile.call(this, converted, filename)
+  } catch (e) {
+    process.emitWarning(`Unable to convert ${filename}:\n ${e}`)
+    return _compile.call(this, content, filename)
+  }
 }
 
 function restore () {
