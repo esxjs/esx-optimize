95059a74168f056974bb4ce204af8353e7f74c73
David Mark Clements <huperekchuno@googlemail.com>
2019-06-27 16:27:02 +0200

    bug fix, do not crash with native modules

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 'use strict'
 const convert = require('to-esx')
 const Module = require('module')
+const { extname } = require('path')
 const { _compile } = Module.prototype
 require('esx') //load esx into the cache
 const esx = require.resolve('esx')
 
 const preloaded = require.main === undefined
 
+function unsupported (filename) {
+  const ext = extname(filename)
+  return ext !== '.js' || ext !== '.mjs'
+}
+
 function install (opts = {}) {
   const { exclude = () => false } = opts
   Module.prototype._compile = function (content, filename) {
-    if (filename === esx || exclude(filename)) {
+    if (filename === esx || exclude(filename) || unsupported(filename)) {
       return _compile.call(this, content, filename)
     }
-    const converted = convert(content)
-    return _compile.call(this, converted, filename)
+    try { 
+      const converted = convert(content)
+      return _compile.call(this, converted, filename)
+    } catch (e) {
+      console.log(e, filename, content)
+    }
+    return content
   }
   return module.exports
 }
